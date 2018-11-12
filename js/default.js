4acf9e568dc053107c68722dd4d9c81d42e5004d
MarinaMariMa <38421141+MarinaMariMa@users.noreply.github.com>
2018-11-12 17:39:40 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,17 @@
-var result = window.confirm('準備OK?');
+var number = Math.floor(Math.random()*10);
+var answer = parseInt(window.prompt('数あてゲーム☆彡0~10の数字を入れてね☆彡'));
 
-document.getElementById('choice').textContent = result;
+var message;
+if(answer === number){
+ message = 'あたり！';
+}else if (answer < number){
+ message = '残念！もっとおおきいよ！';
+}else if (number < answer){
+ message = '残念！もっとちいさいよ～';
+}else{
+ message = '0~10までの数字を入力してね';
+}
+
+
+
+document.getElementById('choice').textContent = message;
